459e96c44ed9a35ede999e535dfa2828481a1d04
Maxime-Le-Codeur <maximecunche@gmail.com>
2024-12-08 14:43:05 +0100

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\mcunche duarte\\Desktop\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\mcunche duarte\\Desktop\\Appli_du_goat\\flutter_application_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\mcunche duarte\\Desktop\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\mcunche duarte\\Desktop\\Appli_du_goat\\flutter_application_1"
+  "FLUTTER_ROOT=C:\\Users\\mcunche duarte\\Desktop\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\mcunche duarte\\Desktop\\Appli_du_goat\\flutter_application_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\mcunche duarte\\Desktop\\Appli_du_goat\\flutter_application_1"
+  "FLUTTER_TARGET=C:\\Users\\mcunche duarte\\Desktop\\Appli_du_goat\\flutter_application_1\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\mcunche duarte\\Desktop\\Appli_du_goat\\flutter_application_1\\.dart_tool\\package_config.json"
+)
